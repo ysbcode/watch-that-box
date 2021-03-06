057820cb02aaf4fb22c6c97399dd428b4b352a25
ysbcode <ysbcode@gmail.com>
2021-03-06 13:22:22 -0500

    create remote repo

---
+++ README.md
@@ -0,0 +1,25 @@
+# Watch Than Box! #
+
+## Acitivity ##
+Make a box appear on the screen and create buttons with different functions to change appearance of the box
+
+## Result ##
+1. Created box in html and defined default characteristics and position of the box
+2. Created buttons for different functions
+3. Using javascript assigned the functions to each button
+4. Reset button reverses every characteristic to the default
+
+## Functions Include ##
+* Grow - Grows the box in size
+* Blue - Changes box color to Blue
+* Fade - Fades the box color
+* Left - Move box to left
+* Right - Move box to right
+* Top - Move box to top
+* Bottom - Move box to bottom
+* Reset - Reset box characteristics and postion
+
+## Languages Used ##  
+HTML and Javascript
+
+
---
+++ javascript.js
@@ -0,0 +1,49 @@
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px";
+
+});
+document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.backgroundColor = "blue";
+
+});
+document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.opacity = "0.4";
+
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.left = "-100px";
+
+});
+
+document.getElementById("button5").addEventListener("click", function(){
+
+    document.getElementById("box").style.left = "100px";
+
+});
+
+document.getElementById("button6").addEventListener("click", function(){
+
+    document.getElementById("box").style.top = "-100px";
+
+});
+
+document.getElementById("button7").addEventListener("click", function(){
+
+    document.getElementById("box").style.top = "100px";
+
+});
+
+document.getElementById("button8").addEventListener("click", function(){
+    document.getElementById("box").style.height = "150px"; 
+    document.getElementById("box").style.backgroundColor = "orange";
+    document.getElementById("box").style.opacity = "1"; 
+    document.getElementById("box").style.margin = "150px";
+    document.getElementById("box").style.position = "relative"; 
+    document.getElementById("box").style.left = "0px";
+    document.getElementById("box").style.top = "0px";
+});  
